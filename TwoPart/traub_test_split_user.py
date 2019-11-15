fee99e9a28a3479700ea473ce9b4b2a2574c91b5
wumutong <wumutong121@163.com>
2019-11-15 19:43:52 +0800

    调用自己编写的train_test_split方法，并查看准确率

--- TwoPart/traub_test_split_user.py
+++ TwoPart/traub_test_split_user.py
@@ -1,3 +1,4 @@
+import numpy as np
 from sklearn import datasets
 
 from  TwoPart.train_test_split import train_test_split
@@ -5,24 +6,25 @@ iris = datasets.load_iris()
 
 X = iris.data
 y = iris.target
+
+
+#调用我们编写的随机取样测试方法
 X_train, X_test, y_train, y_test = train_test_split(X, y)
+#输出我们我编写的随机取样测试方法
 print(X_train.shape)
 print(X_test.shape)
 print(y_train.shape)
 print(y_test.shape)
 
-
 from OnePart.OnePartDemoPlus import OnePartDemoPlus
-#使用我们knn(上)所创建的工具类
-my_kNNClassifier = OnePartDemoPlus(k=3)
-#初始化相关训练集
-my_kNNClassifier.fit(X_train,y_train)
 #测试
-y_predict = my_kNNClassifier.predict(X_test)
+knn_clf = OnePartDemoPlus(k=3)
+knn_clf.fit(X_train, y_train)
+y_predict = knn_clf.predict(X_test)
+# 比对y_predict和y_test结果是否一致
+print(sum(y_predict == y_test) / len(y_test))#0.9955555555555555
 
-# 两个向量的比较，返回一个布尔型向量，对这个布尔向量（false=1，true=0）sum，
-print(sum(y_predict == y_test)) #8
-print(sum(y_predict == y_test)/len(y_test)) #0.26666666666666666
+
 
 
 
