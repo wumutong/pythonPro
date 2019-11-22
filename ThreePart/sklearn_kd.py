bbdbb363e12c74f08c070dd9b71e29acb1b0a38b
wumutong <wumutong121@163.com>
2019-11-22 18:18:21 +0800

    使用sklearn 中的 kd 方法

---
+++ ThreePart/sklearn_kd.py
@@ -0,0 +1,31 @@
+import numpy as np
+from matplotlib import pyplot as plt
+from matplotlib.patches import Circle
+from sklearn.neighbors import KDTree
+np.random.seed(0)
+points = np.random.random((100, 2))
+tree = KDTree(points)
+point = points[0]
+# kNN
+dists, indices = tree.query([point], k=3)
+print(dists, indices)
+# query radius"半径"
+indices = tree.query_radius([point], r=0.2)
+print(indices)
+fig = plt.figure()
+ax = fig.add_subplot(111, aspect='equal')
+ax.add_patch(Circle(point, 0.2, color='r', fill=False))
+X, Y = [p[0] for p in points], [p[1] for p in points]
+plt.scatter(X, Y)
+plt.scatter([point[0]], [point[1]], c='r')
+plt.show()
+
+
+
+
+
+
+
+
+
+
